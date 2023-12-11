9d8a0af98a6103e2f84581a1739dbb1d9c3fb048
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-12-11 15:30:28 -0500

    fix error

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, make_response, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db # import your models here!
+from models import db, Book, Publisher, Author # import your models here!
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -20,6 +20,70 @@ def index():
     return "Hello world"
 
 # write your routes here!
+
+@app.get('/authors')
+def get_authors():
+    author = Author.query.all()
+    
+    data = [a.to_dict() for a in author]
+    return make_response(jsonify(data), 200)
+
+@app.get('/authors/<int:id>')
+def get_author_by_id(id):
+    author = db.session.get(Author, id)
+    if not author:
+        return make_response(jsonify({'error': 'Shumting Wong! no author'}), 404)
+    
+    return make_response(jsonify(author.to_dict(rules=('-publishers'))), 200)
+
+@app.delete('/authors/<int:id>')
+def delete_author(id):
+    author = db.session.get(Author, id)
+    if not author:
+        return make_response(jsonify({'error': 'Shumting Wong! no author'}), 404)
+    
+    db.session.delete(author)
+    db.session.commit()
+    return make_response(jsonify({}), 204)
+
+@app.get('/books')
+def get_books():
+    books = Book.query.all()
+    
+    book_dict = books.to_dict(rules=('-publishers', '-authors'))
+    return make_response(jsonify(book_dict), 200)
+
+@app.post('/books')
+def post_book():
+    data = request.json
+    
+    try:
+        book = Book(
+            name = data.get('name'),
+            page_count = data.get('page_count'),
+            author_name = data.get('author_name'),
+            publisher_name = data.get('publisher_name')
+        )
+        db.session.add(book)
+        db.session.commit()
+        return make_response(jsonify(book.to_dict(rules=('-publishers', '-authors'))), 201)
+    except:
+        return make_response(jsonify({'error': 'Shumting Wong'}), 400)
+
+@app.get('/publishers')
+def get_publishers():
+    pub = Publisher.query.all()
+    
+    return make_response(jsonify(p.to_dict() for p in pub), 200)
+
+@app.get('/publishers/<int:id>')
+def get_publisher_by_id(id):
+    pub = db.session.get(Publisher, id)
+    
+    if not pub:
+        return make_response(jsonify({'error': 'no publisher'}), 404)
+    
+    return make_response(jsonify(pub.to_dict(rules=('-books'))), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -2,6 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -10,3 +12,56 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # write your models here!
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'book_table'
+    
+    serialize_rules = ('-publishers.books',)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True, nullable=False)
+    page_count = db.Column(db.Integer, nullable=False)
+    
+    author_name = db.Column(db.String, db.ForeignKey('author_table.title'), nullable=False)
+    publisher_name = db.Column(db.String, db.ForeignKey('publisher_table.title'), nullable=False)
+    
+    publishers = db.relationship('Publisher', back_populates='books', cascade='all, delete-orphan')
+    authors = association_proxy('publishers', 'authors')
+    
+    @validates('page_count')
+    def validate_page_count(self, key: int, value: int):
+        if value <= 0:
+            raise ValueError('at least one page pleaseee')
+        return value
+    
+class Publisher(db.Model, SerializerMixin):
+    __tablename__ = 'publisher_table'
+    
+    serialize_rules = ('-authors.publishers', '-books.publishers')
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    founding_year = db.Column(db.Integer, nullable=False)
+    
+    authors = db.relationship('Author', back_populates='publishers')
+    books = db.relationship('Book', back_populates='publishers')
+    
+    @validates('founding_year')
+    def validate_founding_year(self, key: int, value: int):
+        
+        current_year = datetime.now().year
+        if 1600 <= value <= current_year:
+            return value
+        
+        raise ValueError('Wrong year!')
+    
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'author_table'
+    
+    serialize_rules = ('-publishers.author',)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    pen_name = db.Column(db.String)
+    
+    publishers = db.relationship('Publisher', back_populates='authors', cascade='all, delete-orphan')
+    books = association_proxy('publishers', 'books')
